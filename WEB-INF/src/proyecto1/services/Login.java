dbbcb07f842abce4ec069b1c4d9326041d21d7b8
Alfanarwar <cafrock@gmail.com>
2016-09-27 11:55:34 -0500

    Create Login.java

---
+++ WEB-INF/src/proyecto1/services/Login.java
@@ -0,0 +1,25 @@
+package proyecto1.services;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import proyecto1.database.Connector;
+
+@SuppressWarnings("serial")
+public class Login extends HttpServlet{
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+    throws ServletException, IOException {
+        
+    }
+    public void doPost(HttpServletRequest  request,
+                       HttpServletResponse response)
+    throws ServletException, IOException {
+        
+    }
+}
